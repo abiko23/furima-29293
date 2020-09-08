448f03879dac1e628cfb7c0a0f41a92648caa32e
abiko23 <62099230+abiko23@users.noreply.github.com>
2020-09-08 19:40:15 +0900

    DBmigrate

--- db/migrate/20200908101212_devise_create_users.rb
+++ db/migrate/20200908101212_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :fullname_first,     null: false
+      t.string :fullname_last,      null: false
+      t.string :kananame_first,     null: false
+      t.string :kananame_last,      null: false
+      t.integer :birth,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
