adf5137cca3f7843e997ba6045f42b7931f26fbd
abiko23 <62099230+abiko23@users.noreply.github.com>
2020-09-16 12:40:39 +0900

    DBmigrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname, :email, :password, :fullname_first, :fullname_last, :kananame_first, :kananame_last, :birth, presence: true
   validates :kananame_first, :kananame_last, format: { with: /\A[ァ-ン]/, base: 'お名前(カナ)はカタカナで入力してください。' }
   validates :fullname_first, :fullname_last, format: { with: /\A[ぁ-んァ-ン一-龥]/, base: 'お名前は全角で入力してください。'}
