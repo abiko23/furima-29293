e27054035c0dbcc1964809f2608d55feacd2aed1
abiko23 <62099230+abiko23@users.noreply.github.com>
2020-09-22 16:41:56 +0900

    fix1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update, :new, :post]
   root to: 'items#index'
-  resources :items, only: [:edit, :new, :create, :post, :show] 
+  resources :items, only: [:edit, :new, :create, :post] 
 
 end
